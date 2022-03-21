b5b638c013e78971d29cb5afe5ee91d66670c71d
dandyraka <dandyraka13@gmail.com>
2022-03-21 22:06:53 +0700

    All downloads goes to 'videos' folder

--- index.js
+++ index.js
@@ -8,41 +8,13 @@ import chalk from 'chalk'
     const browser = await puppeteer.launch({
         headless: true,
         args: [
-            '--autoplay-policy=user-gesture-required',
-            '--disable-background-networking',
-            '--disable-background-timer-throttling',
-            '--disable-backgrounding-occluded-windows',
-            '--disable-breakpad',
-            '--disable-client-side-phishing-detection',
-            '--disable-component-update',
-            '--disable-default-apps',
-            '--disable-dev-shm-usage',
-            '--disable-domain-reliability',
-            '--disable-extensions',
-            '--disable-features=AudioServiceOutOfProcess',
-            '--disable-hang-monitor',
-            '--disable-ipc-flooding-protection',
-            '--disable-notifications',
-            '--disable-offer-store-unmasked-wallet-cards',
-            '--disable-popup-blocking',
-            '--disable-print-preview',
-            '--disable-prompt-on-repost',
-            '--disable-renderer-backgrounding',
-            '--disable-setuid-sandbox',
-            '--disable-speech-api',
-            '--disable-sync',
-            '--hide-scrollbars',
-            '--ignore-gpu-blacklist',
-            '--metrics-recording-only',
-            '--mute-audio',
-            '--no-default-browser-check',
-            '--no-first-run',
-            '--no-pings',
             '--no-sandbox',
-            '--no-zygote',
-            '--password-store=basic',
-            '--use-gl=swiftshader',
-            '--use-mock-keychain',
+            '--disable-setuid-sandbox',
+            '--aggressive-cache-discard',
+            '--disable-cache',
+            '--disable-application-cache',
+            '--disable-offline-load-stale-cache',
+            '--disk-cache-size=0',
             '--incognito',
         ]
     });
@@ -73,10 +45,11 @@ import chalk from 'chalk'
     console.log(chalk.green("Done!"));
     let media = await page.$x('//*[@id="app"]/div[2]/div[2]/div/div[2]/div[2]/div/div')
     console.log(chalk.green("\nTotal videos:", media.length));
-    if (!fs.existsSync(username)) fs.mkdirSync(username)
+    if (!fs.existsSync(`videos`)) fs.mkdirSync(`videos`)
+    if (!fs.existsSync(`videos/${username}`)) fs.mkdirSync(`videos/${username}`)
     for (let i = 1; i <= media.length; i++) {
         const link = await page.evaluate(el => el.href, (await page.$x(`//*[@id="app"]/div[2]/div[2]/div/div[2]/div[2]/div/div[${i}]/div[1]/div/div/a`))[0])
-        console.log(`[${i}.] ${link}`)
+        console.log(`[${i}] ${link}`)
         let filename = link.split("/").pop()+".mp4";
         let getNowm = await axios.get(`https://server1.majhcc.xyz/api/tk?url=${link}`).then(resp => {
             if(resp.data.success) return resp.data.link;
@@ -86,7 +59,7 @@ import chalk from 'chalk'
                 responseType: "stream"
             })
             .then(response => {
-                response.data.pipe(fs.createWriteStream(`${username}/${filename}`));
+                response.data.pipe(fs.createWriteStream(`videos/${username}/${filename}`));
                 console.log(chalk.green(`[+] Download successfully (${filename})\n`));
             })
             .catch(error => {
